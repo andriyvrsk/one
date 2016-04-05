9edd29513c82a0191f95db730d21b4a5bebca226
andriyvrsk <andriyvrsk@gmail.com>
2016-04-05 19:39:12 +0300

    Class Table added

--- TestProject/src/main/java/com/logos/study/Table.java
+++ TestProject/src/main/java/com/logos/study/Table.java
@@ -1,5 +1,17 @@
 package com.logos.study;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Table {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Long id;
+	
+	String firstName;
+	String lastName;
 }
